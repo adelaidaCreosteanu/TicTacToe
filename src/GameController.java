b715c1368f2698df8bc716350bb77c34d8183140
Heidi <adelaida.creosteanu@gmail.com>
2019-08-23 13:17:17 +0200

    [feat] Playfield printer
    GameController owns a printer, which shows the board to the user.

--- src/GameController.java
+++ src/GameController.java
@@ -1,12 +1,24 @@
 
 public class GameController {
-    Board board;
+    private Board board;
+    private static Printer printer = new Printer();
 
     public GameController() {
         board = new Board(3);
     }
 
-    public void play () {
-        System.out.println("Let's play!");
+    public void play() {
+        System.out.println("The current state of the playing field is:");
+        showBoard();
+        System.out.println("Player 1 made a move to 2, 2!");
+        board.addMove(1, new Position(1, 1));
+        showBoard();
+        System.out.println("Player 2 made a move to 1, 2");
+        board.addMove(2, new Position(0, 1));
+        showBoard();
+    }
+
+    public void showBoard() {
+        printer.print(board.getGrid());
     }
 }
---
+++ src/Printer.java
@@ -0,0 +1,55 @@
+public class Printer {
+    private int[][] grid;
+
+    public Printer() {
+    }
+
+    public void print(int [][] grid) {
+        this.grid = grid;
+
+        int i, j;
+        for (i = 0; i < grid.length - 1; i++) {
+            for (j = 0; j < grid.length - 1; j++) {
+                printRowCol(i, j);
+            }
+            printRowLastCol(i, j);
+        }
+
+        for (j = 0; j < grid.length - 1; j++) {
+            printLastRowCol(i, j);
+        }
+        printLastRowLastCol(i, j);
+    }
+
+    private void printRowCol(int i, int j) {
+        if (grid[i][j] == 0) {
+            System.out.print("_|");
+        } else {
+            System.out.print(grid[i][j] + "|");
+        }
+    }
+
+    private void printRowLastCol(int i, int j) {
+        if (grid[i][j] == 0) {
+            System.out.println("_");
+        } else {
+            System.out.println(grid[i][j]);
+        }
+    }
+
+    private void printLastRowCol(int i, int j) {
+        if (grid[i][j] == 0) {
+            System.out.print(" |");
+        } else {
+            System.out.print(grid[i][j] + "|");
+        }
+    }
+
+    private void printLastRowLastCol(int i, int j) {
+        if (grid[i][j] == 0) {
+            System.out.println(" ");
+        } else {
+            System.out.println(grid[i][j]);
+        }
+    }
+}
