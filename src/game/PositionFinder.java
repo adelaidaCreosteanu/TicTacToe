d0028d7586942e5870ea697b819aaec53e859546
Heidi <adelaida.creosteanu@gmail.com>
2019-09-02 12:06:38 +0200

    Extract some AI functionality into PositionFinder
    StrategicAI now has only one (higher) abstraction level. PositionFinder has the lower abstraction level about creating specific Positions.

---
+++ src/game/PositionFinder.java
@@ -0,0 +1,127 @@
+package game;
+
+/**
+ * This class is used by StrategicAI to find strategic positions.
+ */
+public class PositionFinder {
+    private Player[][] grid;
+    private int size;
+
+    public PositionFinder(Player[][] grid) {
+        this.grid = grid;
+        this.size = grid.length;
+    }
+
+    public Position fillRowOrColumn() {
+        return completeRowOrColumn(Player.AI);
+    }
+
+    public Position fillDiagonal() {
+        return completeDiagonal(Player.AI);
+    }
+
+    public Position blockRowOrColumn() {
+        Position move = completeRowOrColumn(Player.ONE);
+        if (move == null) move = completeRowOrColumn(Player.TWO);
+        return move;
+    }
+
+    public Position blockDiagonal() {
+        Position move = completeDiagonal(Player.ONE);
+        if (move == null) move = completeDiagonal(Player.TWO);
+        return move;
+    }
+
+    private Position completeRowOrColumn(Player filler) {
+        Position move = null;
+        for (int i = 0; i < size && move == null; i++) {
+            move = completeRow(filler, i);
+            if (move == null) move = completeColumn(filler, i);
+        }
+        return move;
+    }
+
+    private Position completeRow(Player filler, int row) {
+        int symbols = 0;
+        boolean otherPlayer = false;
+        Position move = null;
+
+        for (int j = 0; j < size; j++) {
+            if (grid[row][j] == filler) {
+                symbols++;
+            } else if (grid[row][j] != null) { // Another player on this row
+                otherPlayer = true;
+            } else {
+                move = new Position(row, j);
+            }
+        }
+
+        if (symbols == size - 1 && !otherPlayer) return move;
+        return null;
+    }
+
+    private Position completeColumn(Player filler, int col) {
+        int symbols = 0;
+        boolean otherPlayer = false;
+        Position move = null;
+
+        for (int i = 0; i < size; i++) {
+            if (grid[i][col] == filler) {
+                symbols++;
+            } else if (grid[i][col] != null) { // Another player on this row
+                otherPlayer = true;
+            } else {
+                move = new Position(i, col);
+            }
+        }
+
+        if (symbols == size - 1 && !otherPlayer) return move;
+        return null;
+    }
+
+    private Position completeDiagonal(Player filler) {
+        Position move = completePrimaryDiagonal(filler);
+        if (move == null) move = completeSecondaryDiagonal(filler);
+
+        return move;
+    }
+
+    private Position completePrimaryDiagonal(Player filler) {
+        int symbols = 0;
+        boolean otherPlayer = false;
+        Position move = null;
+
+        for (int i = 0; i < size; i++) {
+            if (grid[i][i] == filler) {
+                symbols++;
+            } else if (grid[i][i] != null) {
+                otherPlayer = true;
+            } else {
+                move = new Position(i, i);
+            }
+        }
+
+        if (symbols == size - 1 && !otherPlayer) return move;
+        return null;
+    }
+
+    private Position completeSecondaryDiagonal(Player filler) {
+        int symbols = 0;
+        boolean otherPlayer = false;
+        Position move = null;
+
+        for (int i = 0; i < size; i++) {
+            int j = size - i - 1;
+            if (grid[i][j] == filler) {
+                symbols++;
+            } else if (grid[i][j] != null) {
+                otherPlayer = true;
+            } else {
+                move = new Position(i, j);
+            }
+        }
+
+        if (symbols == size - 1 && !otherPlayer) return move;
+        return null;
+    }
+}
--- src/game/StrategicAI.java
+++ src/game/StrategicAI.java
@@ -9,6 +9,7 @@ import java.util.Random;
 public class StrategicAI extends AI {
     private int size;
     private Player[][] grid;
+    private PositionFinder finder;
 
     public StrategicAI(Board board) {
         super(board);
@@ -18,6 +19,8 @@ public class StrategicAI extends AI {
     @Override
     public Position makeMove() {
         grid = board.getGrid();
+        finder = new PositionFinder(grid);
+
         Position move = makeWin();
         if (move == null) move = makeBlock();
         if (move == null) move = makeAnyMove();
@@ -26,116 +29,17 @@ public class StrategicAI extends AI {
     }
 
     private Position makeWin() {
-        Position move = fillRowOrColumn(Player.AI);
-        if (move == null) move = fillDiagonal(Player.AI);
+        Position move = finder.fillRowOrColumn();
+        if (move == null) move = finder.fillDiagonal();
         return move;
     }
 
     private Position makeBlock() {
-        Position move = fillRowOrColumn(Player.ONE);
-        if (move == null) move = fillRowOrColumn(Player.TWO);
-        if (move == null) move = fillDiagonal(Player.ONE);
-        if (move == null) move = fillDiagonal(Player.TWO);
+        Position move = finder.blockRowOrColumn();
+        if (move == null) move = finder.blockDiagonal();
         return move;
     }
-
-    private Position fillRowOrColumn(Player filler) {
-        Position move = null;
-        for (int i = 0; i < size && move == null; i++) {
-            move = fillRow(filler, i);
-            if (move == null) move = fillColumn(filler, i);
-        }
-        return move;
-    }
-
-    private Position fillRow(Player filler, int row) {
-        int symbols = 0;
-        boolean otherPlayer = false;
-        Position move = null;
-
-        for (int j = 0; j < size; j++) {
-            if (grid[row][j] == filler) {
-                symbols++;
-            } else if (grid[row][j] != null) { // Another player on this row
-                otherPlayer = true;
-            } else {
-                move = new Position(row, j);
-            }
-        }
-
-        // if can win on this row
-        if (symbols == size - 1 && !otherPlayer) return move;
-        return null;
-    }
-
-    private Position fillColumn(Player filler, int col) {
-        int symbols = 0;
-        boolean otherPlayer = false;
-        Position move = null;
-
-        for (int i = 0; i < size; i++) {
-            if (grid[i][col] == filler) {
-                symbols++;
-            } else if (grid[i][col] != null) { // Another player on this row
-                otherPlayer = true;
-            } else {
-                move = new Position(i, col);
-            }
-        }
-
-        // if can win on this column
-        if (symbols == size - 1 && !otherPlayer) return move;
-        return null;
-    }
-
-    private Position fillDiagonal(Player filler) {
-        Position move = fillPrimaryDiagonal(filler);
-        if (move == null) move = fillSecondaryDiagonal(filler);
-
-        return move;
-    }
-
-    private Position fillPrimaryDiagonal(Player filler) {
-        int symbols = 0;
-        boolean otherPlayer = false;
-        Position move = null;
-
-        for (int i = 0; i < size; i++) {
-            if (grid[i][i] == filler) {
-                symbols++;
-            } else if (grid[i][i] != null) {
-                otherPlayer = true;
-            } else {
-                move = new Position(i, i);
-            }
-        }
-
-        // if can win on this diagonal
-        if (symbols == size - 1 && !otherPlayer) return move;
-        return null;
-    }
-
-    private Position fillSecondaryDiagonal(Player filler) {
-        int symbols = 0;
-        boolean otherPlayer = false;
-        Position move = null;
-
-        for (int i = 0; i < size; i++) {
-            int j = size - i - 1;
-            if (grid[i][j] == filler) {
-                symbols++;
-            } else if (grid[i][j] != null) {
-                otherPlayer = true;
-            } else {
-                move = new Position(i, j);
-            }
-        }
-
-        // if can win on this diagonal
-        if (symbols == size - 1 && !otherPlayer) return move;
-        return null;
-    }
-
+    
     private Position makeAnyMove() {
         Position move = null;
         if (centerIsEmpty()) {
@@ -147,7 +51,7 @@ public class StrategicAI extends AI {
     }
 
     private boolean centerIsEmpty() {
-        return size % 2 == 1 && grid[size/2][size/2] == null;
+        return size % 2 == 1 && grid[size / 2][size / 2] == null;
     }
 
     private Position randomMove() {
